6ef901094a6c3e77b123d092ff521a7dcb742cdc
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-08-11 01:14:13 +0530

    Setting Up The User Interface

---
+++ app/src/main/java/com/codeenemy/memorywhisper/AddHappyPlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.codeenemy.memorywhisper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+    }
+}
