4647855828d1070facbef312f558a623e8e74dd3
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-08-13 23:22:59 +0530

    Setup UI

--- app/src/main/java/com/codeenemy/memorywhisper/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/codeenemy/memorywhisper/AddHappyPlaceActivity.kt
@@ -2,10 +2,22 @@ package com.codeenemy.memorywhisper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codeenemy.memorywhisper.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    private var binding: ActivityAddHappyPlaceBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
--- app/src/main/java/com/codeenemy/memorywhisper/MainActivity.kt
+++ app/src/main/java/com/codeenemy/memorywhisper/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.codeenemy.memorywhisper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codeenemy.memorywhisper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.fabAddHappyPlace?.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
